5a10b54a9c12dc633cb448394aa92560a6ceaa0c
Diogo Sousa <diogogsousa@gmail.com>
2013-05-03 16:06:46 +0100

    Added star free regex AST

---
+++ src/x/starFreeRegExp/StarFreeRegExpEmpty.java
@@ -0,0 +1,27 @@
+package x.startFreeRegExp;
+
+import java.util.Set;
+import java.util.List;
+
+import java.util.HashSet;
+
+public class StarFreeRegExpEmpty
+    extends StarFreeRegExp
+{
+    public StarFreeRegExpEmpty()
+    {
+
+    }
+
+    @Override
+    public Set<List<String>> getWords()
+    {
+        return new HashSet<List<String>>();
+    }
+
+    @Override
+    public String toString()
+    {
+        return "∅";
+    }
+}
