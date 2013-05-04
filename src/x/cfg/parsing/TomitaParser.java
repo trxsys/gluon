d16be432e14bbfd70a3a6eaa7bac34091bc05903
Diogo Sousa <diogogsousa@gmail.com>
2013-05-04 15:34:52 +0100

    Started tomita parsing algorithm implementation

---
+++ src/x/cfg/parsing/TomitaParser.java
@@ -0,0 +1,22 @@
+package x.cfg.parsing;
+
+import java.util.List;
+
+import x.cfg.Terminal;
+
+public class TomitaParser
+{
+    private static final boolean DEBUG=true;
+
+    private final ParsingTable table;
+
+    public TomitaParser(ParsingTable t)
+    {
+        table=t;
+    }
+
+    public void parse(List<String> string)
+    {
+        
+    }
+}
