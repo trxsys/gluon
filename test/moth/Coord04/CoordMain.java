ea37fb8bb2164c573950a14f0431c2713c4ebaff
Diogo Sousa <diogogsousa@gmail.com>
2013-07-17 23:58:45 +0100

    Adapted CoordMain test.

--- test/moth/Coord04/CoordMain.java
+++ test/moth/Coord04/CoordMain.java
@@ -1,47 +1,59 @@
 package test.moth.Coord04;
 
 import test.common.Atomic;
+import test.common.Contract;
+
+@Contract(clauses = "resetX resetY;"
+                   +"resetY resetX;")
+class Coord {
+    private int x, y;
+
+    public Coord()
+    {
+        x=y=0;
+    }
+    
+    @Atomic
+    public void swap() {
+        int oldX = x;
+        x = y; // swap X
+        y = oldX; // swap Y
+    }
+    
+    @Atomic
+    public void resetX(){
+        x = 0;
+    }
+
+    @Atomic
+    public void resetY(){
+        y = 0;
+    }
+}
+
 
 public class CoordMain extends Thread {
-
+    
 	Coord coord;
-
+    
 	public static void main(String[] args) {
 		Coord c = new Coord();
 		new CoordMain(c).start();
 		new CoordMain(c).start();
 	}
-
+    
 	public CoordMain(Coord coord) {
 		this.coord = coord;
 	}
-
+    
 	public void run() {
-		swap();
+		coord.swap();
 		reset();
 	}
-	@Atomic
-	public void swap() {
-		int oldX;
-		oldX = coord.x;
-		coord.x = coord.y; // swap X
-		coord.y = oldX; // swap Y
-	}
-	
-	public void reset() {
-		resetX();
-		// inconsistent state (0, y)
-		resetY();
-	}
-	@Atomic
-	private void resetX(){
-		coord.x = 0;
-	}
-	@Atomic
-	private void resetY(){
-		coord.y = 0;
-	}
-	static class Coord {
-		int x, y;
-	}
+
+    public void reset() {
+        coord.resetX();
+        // inconsistent state (0, y)
+        coord.resetY();
+    }
 }
