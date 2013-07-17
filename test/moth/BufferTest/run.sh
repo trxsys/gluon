9b682aa1aa947e48a7e591004ded2c82fcb34296
Diogo Sousa <diogogsousa@gmail.com>
2013-07-17 23:15:26 +0100

    Removed BufferTest: the program is correct

--- test/moth/BufferTest/run.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-mainClass=test.`basename "$(pwd)"`.BufferMain
-
-if [ "$1" == "-pc" ]; then
-    cd ../../..; ./moth.sh -a --dump process,views,dependency --cp src/ --sensorflag viewconsistency:pc --sensorflag arthoviewconsistency:pc $mainClass
-else
-    cd ../../..; ./moth.sh -a --dump process,views,dependency --cp src/ $mainClass
-fi
