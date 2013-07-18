36880ff398dbf9469abbed96aa61a96a7c46206a
Diogo Sousa <diogogsousa@gmail.com>
2013-07-18 02:00:05 +0100

    Adapted MyStringBuffer test.

--- test/moth/MyStringBuffer/Main.java
+++ test/moth/MyStringBuffer/Main.java
@@ -1,26 +1,41 @@
 package test.moth.MyStringBuffer;
 
 import test.common.Atomic;
+import test.common.Contract;
 
 /*
- * Simulate java.lang.StringBuffer
+ * Simulates java.lang.StringBuffer
  */
-public final class MyStringBuffer {
-
+@Contract(clauses = "length getChars;")
+class MyStringBuffer {
 	private java.lang.StringBuffer buffer;
 	
 	public MyStringBuffer(String string) {
 		this.buffer = new StringBuffer(string);
 	}
 
+	@Atomic
+	public int length() {
+		return this.buffer.length();	//Read buffer
+	}
+
+	@Atomic
+	public void getChars(int srcBegin, int srcEnd, char[] dst, int dstBegin) {
+		this.buffer.getChars(srcBegin, srcEnd, dst, dstBegin);	//Read buffer
+	}
+}
+
+public class Main
+{
 	public static void main(String args[]) {
 		MyStringBuffer ham = new MyStringBuffer("ham");
 		MyStringBuffer burger = new MyStringBuffer("burger");
-		ham.append(burger);
+
+		append(ham, burger);
 		System.out.println(ham);
 	}
 
-	public MyStringBuffer append(MyStringBuffer other) {
+	public static MyStringBuffer append(MyStringBuffer t, MyStringBuffer other) {
 
 		int len = other.length();	//read other.length
 		
@@ -30,23 +45,6 @@ public final class MyStringBuffer {
 		char[] value = new char[len];
 		other.getChars(0, len, value, 0);
 		
-		//added by Vasco
-		delete(0);
-		//added by Vasco		
-		return this;
-	}
-	@Atomic
-	public int length() {
-		return this.buffer.length();	//Read buffer
-	}
-	@Atomic
-	public void getChars(int srcBegin, int srcEnd, char[] dst, int dstBegin) {
-		this.buffer.getChars(srcBegin, srcEnd, dst, dstBegin);	//Read buffer
-	}
-	
-	//added by Vasco
-	@Atomic
-	public void delete(int pos){
-		this.buffer.deleteCharAt(pos);
+		return t;
 	}
 }
