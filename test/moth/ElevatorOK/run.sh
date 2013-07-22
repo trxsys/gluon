8ed680b5019198afec0edd183b76ca51472dec22
Diogo Sousa <diogogsousa@gmail.com>
2013-07-22 15:46:04 +0100

    Added new result.

--- test/moth/ElevatorOK/run.sh
+++ test/moth/ElevatorOK/run.sh
@@ -5,4 +5,4 @@ mainClass=$package.Elevator
 module=$package.Controls
 
 cd ../../..; ./x.sh -pt --classpath . --module "$module" "$mainClass"
-cd ../../..; ./x.sh --no-grammar-opt -pt --classpath . --module "$module" "$mainClass"
+./x.sh --no-grammar-opt -pt --classpath . --module "$module" "$mainClass"
