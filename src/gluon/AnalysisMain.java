trxsys/gluon

commit dcfe0233c03d9c08e30ae3d3efaab4aa7a204591

Arg list is recorded in PPTerminal.

src/gluon/analysis/programBehavior/PPTerminal.java
@@ -24,18 +24,26 @@ import soot.tagkit.LineNumberTag;
 import soot.tagkit.SourceFileTag;
 import soot.tagkit.Tag;
 
+import java.util.ArrayList;
+import java.util.List;
+
 // Represents a call to the module under analysis
 public class PPTerminal
     extends gluon.grammar.Terminal
 {
     private final SootMethod method; // module method
     private final Unit codeUnit;
-
+    /* arguments is null if the arguments are to be ignored.
+     * An null argument represents an argument that should be ignored.
+     */
+    private List<String> arguments;
+    
     public PPTerminal(SootMethod m, Unit u)
     {
         super(m.getName());
         method=m;
         codeUnit=u;
+        arguments=null;
     }
 
     public PPTerminal(String s)
@@ -87,6 +95,19 @@ public class PPTerminal
         return source;
     }
 
+    public List<String> getArguments()
+    {
+        return arguments;
+    }
+
+    public void addArgument(String arg)
+    {
+        if (arguments == null)
+            arguments=new ArrayList<String>(16);
+
+        arguments.add(arg);
+    }
+
     @Override
     public boolean isEOI()
     {
@@ -115,7 +136,32 @@ public class PPTerminal
     @Override
     public PPTerminal clone()
     {
-        return method != null ? new PPTerminal(method,codeUnit)
+        PPTerminal clone=method != null ? new PPTerminal(method,codeUnit)
             : new PPTerminal(super.getName());
+
+        clone.arguments=new ArrayList<String>(arguments.size());
+        clone.arguments.addAll(arguments);
+
+        return clone;
+    }
+
+    @Override
+    public String toString()
+    {
+        String str=getName();
+
+        if (arguments != null)
+        {
+            int i=0;
+
+            str+="(";
+
+            for (String v: arguments)
+                str+=(i++ == 0 ? "" : ",")+(v == null ? "_" : v);
+
+            str+=")";
+        }
+
+        return str;
     }
 }

test/simple/exampleargs/Main.java
@@ -3,10 +3,10 @@ package test.simple.exampleargs;
 import test.common.Atomic;
 import test.common.Contract;
 
-@Contract(clauses=""//"a b c;"
-          //     +"c c;"
-          //                 +"c() c;"
-                 +"c(X);"
+@Contract(clauses="a b c;"
+                 +"c c;"
+                 +"c() c;"
+                 +"c(X) c;"
                  +"c(_) c;"
                  +"c(X,Y) c;"
                  +"c(X,_,Y) c() c(K,L);")

src/gluon/contract/ContractVisitorExtractWords.java
@@ -89,8 +89,19 @@ public class ContractVisitorExtractWords
         List<List<Terminal>> w=new ArrayList<List<Terminal>>(1);
         List<Terminal> word=new ArrayList<Terminal>(1);
         String methodName=node.getMethod().getText();
+        PPTerminal term=new PPTerminal(methodName);
 
-        word.add(new PPTerminal(methodName));
+        for (TId varId: node.getId())
+        {
+            String var=varId.getText();
+
+            if (var.equals("_"))
+                term.addArgument(null);
+            else
+                term.addArgument(var);
+        }
+
+        word.add(term);
         w.add(word);
 
         words.put(node,w);

commit ab4868fe562794bd9a2f884cc5dde9db64f60746

Now ParseTree can return the number of terminals parsed. Fixed bug in terminal parser introduced in dcfe0233c03d9c08e30ae3d3efaab4aa7a204591.

src/gluon/AnalysisMain.java
@@ -130,7 +130,13 @@ public class AnalysisMain
         String r="";
 
         for (int i=0; i < word.size()-1; i++)
-            r+=(i > 0 ? " " : "")+word.get(i).toString();
+        {
+            Terminal term=word.get(i);
+
+            assert term instanceof PPTerminal;
+
+            r+=(i > 0 ? " " : "")+((PPTerminal)term).getFullName();
+        }
 
         return r;
     }
@@ -180,16 +186,30 @@ public class AnalysisMain
                                          List<ParsingAction> actions,
                                          NonTerminal lca)
     {
-        ParseTree ptree=new ParseTree();
+        if (DEBUG)
+        {
+            ParseTree ptree=new ParseTree(word,actions);
 
-        ptree.buildTree(word,actions);
-
-        return lca.equals(ptree.getLCA());
+            ptree.buildTree();
+        
+            return lca.equals(ptree.getLCA());
+        }
+        else
+            return true;
     }
 
-    private boolean argumentsMatch(List<Terminal> contractWord, List<Terminal> word)
+    private boolean argumentsMatch(List<Terminal> word, List<ParsingAction> actions)
     {
-        return true; /* TODO */
+        ParseTree tree=new ParseTree(word,actions);
+        List<Terminal> parsedWord;
+
+        tree.buildTree();
+
+        parsedWord=tree.getTerminals();
+
+        /* TODO */
+
+        return true;
     }
 
     private int checkThreadWordParse(SootMethod thread,
@@ -210,7 +230,7 @@ public class AnalysisMain
         if (reported.contains(lcaMethod))
             return 1;
 
-        if (!argumentsMatch(null,null))
+        if (!argumentsMatch(word,actions))
         {
             gluon.profiling.Profiling.inc("final:discarded-trees-args-not-match");
             return 1;

src/gluon/analysis/programBehavior/PPTerminal.java
@@ -145,8 +145,7 @@ public class PPTerminal
         return clone;
     }
 
-    @Override
-    public String toString()
+    public String getFullName()
     {
         String str=getName();
 

src/gluon/grammar/parsing/parseTree/ParseTree.java
@@ -69,13 +69,16 @@ public class ParseTree
     private static final boolean DEBUG=false;
 
     private List<ParseTreeNode> leafs;
-    
-    public ParseTree()
+    private final List<Terminal> word;
+    private final List<ParsingAction> actions;
+
+    public ParseTree(List<Terminal> word, List<ParsingAction> actions)
     {
-        leafs=null;
+        this.word=word;
+        this.actions=actions;
     }
 
-    public void buildTree(List<Terminal> word, List<ParsingAction> actions)
+    public void buildTree()
     {
         Stack<ParseTreeNode> stack=new Stack<ParseTreeNode>();
         int pos=0;
@@ -115,6 +118,23 @@ public class ParseTree
             }
             else if (a instanceof ParsingActionAccept)
                 assert pos == word.size()-1; /* -1 because of $ */
+    }
+
+    public List<Terminal> getTerminals()
+    {
+        List<Terminal> terminals=new ArrayList<Terminal>(leafs.size());
+
+        assert leafs != null;
+
+        for (ParseTreeNode node: leafs)
+        {
+            assert node.getElem() instanceof Terminal;
+
+            terminals.add((Terminal)node.getElem());
+        }
+
+        return terminals;
+
     }
 
     public NonTerminal getLCA()

test/simple/exampleargs/Main.java
@@ -3,7 +3,7 @@ package test.simple.exampleargs;
 import test.common.Atomic;
 import test.common.Contract;
 
-@Contract(clauses="a b c;"
+@Contract(clauses="a b c(_,_);"
                  +"c c;"
                  +"c(X,Y) c(K,_);")
 class Module