trxsys/gluon

commit eb7ee9eeb110fa25b969a26e427a9ec0233f0243

Fix in atomicity analysis: synch methods were not taken into account.

src/gluon/analysis/atomicity/AtomicityAnalysis.java
@@ -105,7 +105,8 @@ public class AtomicityAnalysis
             }
             else
             {
-                if (nonterm.isSynchBlock())
+                if (nonterm.isSynchBlock()
+                    || nonterm.getMethod().isSynchronized())
                     return true;
             }
         }

commit ea0f4cd7cd9b6b8a67ea263a392308aae75f9615

Fixed bug null deref introduced in eb7ee9eeb110fa25b969a26e427a9ec0233f0243.

src/gluon/analysis/atomicity/AtomicityAnalysis.java
@@ -106,7 +106,8 @@ public class AtomicityAnalysis
             else
             {
                 if (nonterm.isSynchBlock()
-                    || nonterm.getMethod().isSynchronized())
+                    || (nonterm.getMethod() != null
+                        && nonterm.getMethod().isSynchronized()))
                     return true;
             }
         }