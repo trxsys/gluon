trxsys/gluon

commit bff697b88ab80a6ab05bcaec7de3f93afba3059b

Implemented getInitialConfigurations() for ParserSubwords.

src/gluon/parsing/parser/ParserSubwords.java
@@ -16,9 +16,11 @@
 
 package gluon.parsing.parser;
 
+import java.util.Collection;
 import java.util.Queue;
 import java.util.List;
 import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.Set;
 import java.util.HashSet;
 
@@ -28,19 +30,20 @@ import gluon.grammar.Production;
 import gluon.grammar.Symbol;
 import gluon.grammar.NonTerminal;
 import gluon.grammar.Terminal;
+import gluon.grammar.EOITerminal;
+
+import gluon.parsing.parsingTable.ParsingTable;
+import gluon.parsing.parsingTable.parsingAction.*;
 
 /* Subword parser as described in "Substring parsing for arbitrary context-free
  * grammars", J Rekers, W Koorn, 1991.
  */
 public class ParserSubwords
+    extends Parser
 {
     public ParserSubwords(ParsingTable t)
     {
-        /*
-        table=t;
-        parseLifo=null;
-        acceptedLCA=null;
-        */
+        super(t);
     }
 
     private static boolean addNonTerminalsToQueue(Production prod,
@@ -97,7 +100,7 @@ public class ParserSubwords
 
         return false;
     }
-    
+
     /* For the subword parser to be able to parse the grammar every production
      * must generate at least one non-empty word.
      */
@@ -124,4 +127,65 @@ public class ParserSubwords
 
         return !fail;
     }
+
+    /* The algorithm implemented here is from "Substring parsing for arbitrary
+     * context-free grammars" by Jan Rekers and Wilco Koorn.
+     *
+     */
+    @Override
+    protected void reduce(ParserConfiguration parserConf,
+                          ParsingActionReduce reduction)
+    {
+        Production p=reduction.getProduction();
+        int stackSize=parserConf.stackSize();
+
+        System.err.println("stack size: "+stackSize);
+
+        if (stackSize > p.bodyLength())
+            super.reduce(parserConf,reduction);
+        else if (stackSize == p.bodyLength())
+        {
+            // TODO
+        }
+        else
+        {
+            // TODO
+        }
+    }
+
+    @Override
+    protected Collection<ParserConfiguration>
+        getInitialConfigurations(List<Terminal> input)
+    {
+        Collection<ParserConfiguration> configurations;
+
+        configurations=new ArrayList<ParserConfiguration>(128);
+
+        for (int s: super.table.statesReachedBy(input.get(0)))
+        {
+            ParserConfiguration initConfig=new ParserConfiguration();
+
+            /* We have alread read the terminal at input[0], hence the "1" passed
+             * to the ParserStackNode contructor.
+             */
+            initConfig.stackPush(new ParserStackNode(table.getInitialState(),1));
+            initConfig.pos=1; /* we already "read" the first terminal */
+            configurations.add(initConfig);
+        }
+
+        return configurations;
+    }
+
+    @Override
+    public int parse(List<Terminal> input, ParserCallback pcb)
+        throws ParserAbortedException
+    {
+        assert input.size() != 0;
+        assert !(input.get(input.size()-1) instanceof EOITerminal)
+            : "input should not end with $ in the subword parser";
+
+        return super.parse(input,pcb);
+    }
 }
+
+/* TODO: when to accept */

src/gluon/parsing/parser/Parser.java
@@ -212,13 +212,45 @@ public abstract class Parser
             System.out.println(this.getClass().getSimpleName()+": "+s);
     }
 
-    protected abstract void shift(ParserConfiguration parserConf,
-                                  ParsingActionShift shift);
+    protected void shift(ParserConfiguration parserConf,
+                         ParsingActionShift shift)
+    {
+        parserConf.stackPush(new ParserStackNode(shift.getState(),1));
+        parserConf.pos++;
+
+        parserConf.setAction(shift);
+
+        dprintln(parserConf.hashCode()+": shift "+shift.getState());
+    }
     
-    protected abstract void reduce(ParserConfiguration parserConf,
-                                   ParsingActionReduce reduction);
+    protected void reduce(ParserConfiguration parserConf,
+                          ParsingActionReduce reduction)
+    {
+        Production p=reduction.getProduction();
+        int s;
+        int genTerminals=0;
+
+        for (int i=0; i < p.bodyLength(); i++)
+        {
+            genTerminals+=parserConf.stackPeek().generateTerminals;
+            parserConf.stackPop();
+        }
+        
+        s=parserConf.stackPeek().state;
+        
+        parserConf.stackPush(new ParserStackNode(table.goTo(s,p.getHead()),
+                                                 genTerminals));
+        
+        parserConf.setAction(reduction);
+        
+        dprintln(parserConf.hashCode()+": reduce "+p);
+    }
     
-    protected abstract void accept(ParserConfiguration parserConf);
+    protected void accept(ParserConfiguration parserConf)
+    {
+        parserConf.status=ParserStatus.ACCEPTED;
+        dprintln(parserConf.hashCode()+": accept");
+    }
     
     protected ParserConfiguration newParserConfiguration(ParserConfiguration parent)
     {
@@ -286,7 +318,8 @@ public abstract class Parser
         }
     }
     
-    protected abstract Collection<ParserConfiguration> getInitialConfigurations();
+    protected abstract Collection<ParserConfiguration>
+        getInitialConfigurations(List<Terminal> input);
     
     /* Argument input should be an ArrayList for performance reasons. */
     public int parse(List<Terminal> input, ParserCallback pcb)
@@ -298,7 +331,7 @@ public abstract class Parser
         parseLifo=new Stack<ParserConfiguration>();
         acceptedLCA=new HashSet<NonTerminal>();
 
-        for (ParserConfiguration initialConfig: getInitialConfigurations())
+        for (ParserConfiguration initialConfig: getInitialConfigurations(input))
             parseLifo.add(initialConfig);
 
         while (parseLifo.size() > 0)
@@ -313,7 +346,7 @@ public abstract class Parser
             switch (parserConf.status)
             {
             case RUNNING : parseSingleStep(parserConf,input); break;
-            case ACCEPTED: 
+            case ACCEPTED:
                 int z;
                 NonTerminal lca=parserConf.lca;
 

src/gluon/parsing/parser/ParserNormal.java
@@ -54,57 +54,15 @@ public class ParserNormal
     }
 
     @Override
-    protected void shift(ParserConfiguration parserConf,
-                         ParsingActionShift shift)
-    {
-        parserConf.stackPush(new ParserStackNode(shift.getState(),1));
-        parserConf.pos++;
-
-        parserConf.setAction(shift);
-
-        dprintln(parserConf.hashCode()+": shift "+shift.getState());
-    }
-
-    @Override    
-    protected void reduce(ParserConfiguration parserConf,
-                          ParsingActionReduce reduction)
-    {
-        Production p=reduction.getProduction();
-        int s;
-        int genTerminals=0;
-
-        for (int i=0; i < p.bodyLength(); i++)
-        {
-            genTerminals+=parserConf.stackPeek().generateTerminals;
-            parserConf.stackPop();
-        }
-        
-        s=parserConf.stackPeek().state;
-        
-        parserConf.stackPush(new ParserStackNode(table.goTo(s,p.getHead()),
-                                                 genTerminals));
-        
-        parserConf.setAction(reduction);
-        
-        dprintln(parserConf.hashCode()+": reduce "+p);
-    }
-
-    @Override    
-    protected void accept(ParserConfiguration parserConf)
-    {
-        parserConf.status=ParserStatus.ACCEPTED;
-        dprintln(parserConf.hashCode()+": accept");
-    }
-
-    @Override
-    protected Collection<ParserConfiguration> getInitialConfigurations()
+    protected Collection<ParserConfiguration>
+        getInitialConfigurations(List<Terminal> input)
     {
         ParserConfiguration initConfig=new ParserConfiguration();
         Collection<ParserConfiguration> configurations;
 
         initConfig.stackPush(new ParserStackNode(table.getInitialState(),0));
 
-        configurations=new ArrayList<ParserConfiguration>(0);
+        configurations=new ArrayList<ParserConfiguration>(1);
         configurations.add(initConfig);
 
         return configurations;
@@ -114,7 +72,7 @@ public class ParserNormal
     public int parse(List<Terminal> input, ParserCallback pcb)
         throws ParserAbortedException
     {
-       assert input.size() > 0 
+       assert input.size() > 0
             && input.get(input.size()-1) instanceof EOITerminal
             : "input should end with $";
 

commit c2f7edbdce980dd900f24c390311396df232337b

Fixed bug in the getInitialConfigurations() of the subword parser where the initial state pushed onto the stack was wrong.
This bug was introduced in bff697b88ab80a6ab05bcaec7de3f93afba3059b.

src/gluon/parsing/parser/ParserSubwords.java
@@ -197,8 +197,11 @@ public class ParserSubwords
             /* We have alread read the terminal at input[0], hence the "1" passed
              * to the ParserStackNode contructor.
              */
-            initConfig.stackPush(new ParserStackNode(table.getInitialState(),1));
+            initConfig.stackPush(new ParserStackNode(s,1));
             initConfig.pos=1; /* we already "read" the first terminal */
+
+            initConfig.setAction(new ParsingActionShift(s));
+
             configurations.add(initConfig);
         }
 