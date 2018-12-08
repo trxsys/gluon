377a50460ebcfcadf13d2d2c6d0e86e8b93d2992
Diogo Sousa <diogogsousa@gmail.com>
2018-12-08 20:18:14 +0000

    gluon.sh now compiles the tests as well.

--- gluon.sh
+++ gluon.sh
@@ -4,7 +4,10 @@ set -e
 
 cd "$(dirname "$0")"
 
+echo 'Compiling gluon and tests.'
+sbt --warn compileTests
 sbt --warn assembly
+echo 'Compilation done.'
 
 jar=$(find target -name 'gluon-assembly-*.jar' | sort | tail -1)
 
